59b832d0b4c582cf390f71d5b0903d7a3161818e
mary <102292174+marysoh@users.noreply.github.com>
2023-07-24 00:57:19 +0800

    initialise api

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
+        self.wfile.write(message.encode())
+        return
