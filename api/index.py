7f28be6a0ccd33d69a15f7030281cf789e668551
mary <102292174+marysoh@users.noreply.github.com>
2023-07-24 01:19:29 +0800

    update response to json

--- api/index.py
+++ api/index.py
@@ -1,15 +1,17 @@
 from http.server import BaseHTTPRequestHandler
+import json
 from urllib import parse
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
         dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         if "name" in dic:
-            message = "Hello, " + dic["name"] + "!"
+            message = {"message": "Hello, " + dic["name"] + "!"}
         else:
-            message = "Hello, stranger!"
-        self.wfile.write(message.encode())
-        return
+            message = {"message": "Hello, stranger!"}
+        
+        response_json = json.dumps(message)
+        self.wfile.write(response_json.encode())  # Encode the JSON response
