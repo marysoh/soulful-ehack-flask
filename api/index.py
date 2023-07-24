03a2502e7fbfd4dee8014f1f5b1532959c948a5a
mary <102292174+marysoh@users.noreply.github.com>
2023-07-25 01:25:11 +0800

    added webscraping

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ from urllib import parse
 import os
 from pprint import pprint
 import requests
+from bs4 import BeautifulSoup
 
 
 class handler(BaseHTTPRequestHandler):
@@ -12,9 +13,6 @@ class handler(BaseHTTPRequestHandler):
         subscription_key = os.environ.get('BINGSEARCH_API')
         # endpoint = os.environ.get('BINGSEARCH_ENDPOINT')
         endpoint = "https://api.bing.microsoft.com/v7.0/search"
-
-        print(subscription_key)
-        print(endpoint)
 
         # Construct a request
         mkt = 'en-US'
@@ -33,13 +31,33 @@ class handler(BaseHTTPRequestHandler):
             # pprint(response.json())
 
             # can modify number of search results to return based on the speed of web scraping
-            return search_results["webPages"]["value"][0:4]
+            return search_results["webPages"]["value"][0:2]
         except Exception as ex:
             raise ex
             
-    def web_scrape(self, results):
+    def web_scrape(self, url):
         # do something
-        return
+        # url = "https://www.fairprice.com.sg/product/kinder-bueno-chocolate-wafer-bar-milk-43g-303793"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        # class that corresponds to the product description in the webpage
+        results = soup.find_all("div", class_="sc-10zw1uf-16 kZweZH")
+        # results_soup = BeautifulSoup(results, "html.parser")
+        # class that corresponds to ingreident list
+        # ingredients = soup.find("h2", title="Ingredients")
+        div_elements = soup.find_all("div", class_="sc-3zvnd-0 bCYQcy")
+        # Loop through the div elements to find the one containing the h2 with "Ingredients"
+        target_div = None
+        for div_element in div_elements:
+            h2_element = div_element.find("h2", title="Ingredients")
+            if h2_element:
+                target_div = div_element
+                break
+        if target_div != None:
+            formatted_target_div = target_div.prettify()
+            return formatted_target_div
+        else:
+            return "No details"
 
     def do_GET(self):
         print("init")
@@ -50,14 +68,15 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         if "query" in dic:
             # construct search query to narrow options to singapore supermarkets only
-            search_query = "ntuc fairprice" + dic["query"] 
+            # ensure that search results url's include fairprice website and at the product details page
+            search_query = "+https://www.fairprice.com.sg +product +" + dic["query"] 
             results = self.search(search_query)
+            formatted_results = "Products available:\n"
             for result in results:
-                print(result["name"])
-                print(result["url"])
-
+                ingredients = self.web_scrape(result["url"])
+                formatted_results += "Name: " + result["name"] + "\nIngredients in HTML form:\n" + ingredients + "\nURL to product details: " + result["url"] + "\n\n"
             # final format should be "Products available: " + list of product information
-            message = {"results":  results[0]["name"]}
+            message = {"results":  formatted_results}
         else:
             message = {"results":  "Enter a query"}
         
